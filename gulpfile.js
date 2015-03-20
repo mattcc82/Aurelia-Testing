ab9457d7094a9a0be80743e353cd371fc29ce7ac
mattcc82 <mcastle@GR557.gongos.local>
2015-03-20 16:27:59 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// all gulp tasks are located in the ./build/tasks directory
+// gulp configuration is in files in ./build directory
+var gulp      = require('gulp');
+var plumber   = require('gulp-plumber');
+var sass      = require('gulp-sass');
+var webserver = require('gulp-webserver');
+var opn       = require('opn');
+
+var sourcePaths = {
+  styles: ['scss/**/*.scss']
+};
+
+var distPaths = {
+  styles: 'css'
+};
+
+var server = {
+  host: 'localhost',
+  port: '8001'
+}
+
+gulp.task('sass', function () {
+  gulp.src( sourcePaths.styles )
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest( distPaths.styles ));
+});
+
+gulp.task('webserver', function() {
+  gulp.src( '.' )
+    .pipe(webserver({
+      host:             server.host,
+      port:             server.port,
+      livereload:       true,
+      directoryListing: false
+    }));
+});
+
+gulp.task('openbrowser', function() {
+  opn( 'http://' + server.host + ':' + server.port );
+});
+
+gulp.task('watch', function(){
+  gulp.watch(sourcePaths.styles, ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build', 'webserver', 'watch', 'openbrowser']);
